9edfab647642ecbb9877902de2b65ac0a4d3f131
foo74 <foo74>
2016-05-28 13:51:52 -0700

    init

---
+++ pass_gen.c
@@ -0,0 +1,33 @@
+/****************************************************
+ * Read header, pass_gen.h, for details.
+ ****************************************************/
+
+/* Include our main header file.*/
+#include "pass_gen.h"
+
+/* The main function of our program. */
+int main()
+{
+   /* Print the header info. */
+   printf("\n---------------------------------------------------------");
+   printf("\n- Password Generator is a program that build a list of  -");
+   printf("\n- passwords using various transformations. For example, -");
+   printf("\n- turning o's to 0's.                                   -");   
+   printf("\n---------------------------------------------------------\n\n");
+
+   /* Print the info about this program. */
+   printf("This program...\n");
+
+   /* Be a good main function and return 0 because all went fine. */
+   return 0;
+}
+
+
+/* Our functions are defined below here. We could also put them 
+ * in their own files and link them together or #include them.
+ */
+
+/* See header pass_gen.h for description. */
+void func()
+{
+}
---
+++ pass_gen.h
@@ -0,0 +1,19 @@
+/****************************************************
+ * Password generator.
+ * A program that generates a list of paswords based
+ * on a set of input.
+ *****************************************************/
+
+/* Include the standard io library.*/
+#include <stdio.h>
+
+/* Define the vars we will use to test so we can
+ * easily change them. 
+ */
+#define MAX 1024
+
+/* Function that takes two string pointers s and t. The t
+ * a:    desc.
+ * b:    desc.
+ */
+void func();
