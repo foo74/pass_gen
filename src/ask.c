0eea57739fcd12682a0d998a5248f0474aacf065
foo74 <foo74>
2016-06-07 21:58:09 -0700

    add ask.c

---
+++ src/ask.c
@@ -0,0 +1,27 @@
+#include "ask.h"
+
+
+int main()
+{
+   FILE *input_stream;
+   FILE *output_stream;
+   char buf_in[100];
+   char buf_out[100];
+
+   input_stream = fopen("questions", "r");
+   output_stream = fopen("output", "w+");
+
+   while (fgets(buf_in, sizeof(buf_in), input_stream))
+   {
+      buf_in[strcspn(buf_in, "\n")] = 0;
+      printf("%s ", buf_in);
+      /* The %100s reads only up to 100 chars plus NULL terminator. */
+      scanf("%100s", buf_out);
+      fprintf(output_stream, "%s\n", buf_out);
+   }
+
+   fclose(input_stream);
+   fclose(output_stream);
+
+   return 0;
+}
